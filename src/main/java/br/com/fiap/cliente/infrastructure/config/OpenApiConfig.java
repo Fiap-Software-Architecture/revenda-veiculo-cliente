18144f2b30fe2061ff9284d474f03a3a668db665
Gabriel Aguilera <scs.gabriel@gmail.com>
2026-01-01 18:34:02 -0300

    feat: integracao com keacloak

--- src/main/java/br/com/fiap/cliente/infrastructure/config/OpenApiConfig.java
+++ src/main/java/br/com/fiap/cliente/infrastructure/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package br.com.fiap.cliente.infrastructure.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +13,18 @@ public class OpenApiConfig {
 
     @Bean
     OpenAPI clienteOpenAPI() {
+        final String schemeName = "bearerAuth";
+
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(schemeName))
+                .components(new Components().addSecuritySchemes(
+                        schemeName,
+                        new SecurityScheme()
+                                .name(schemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                ))
                 .info(new Info()
                         .title("Revenda de Veículos API")
                         .version("v1")
